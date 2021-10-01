58dcd88a0beca9892b7119918c90a29e46366504
garrettbadger <garrett.badger.id@gmail.com>
2021-10-01 11:30:53 -0600

    added the game to the main

--- welcome/main.py
+++ welcome/main.py
@@ -1,5 +1,5 @@
 from rowley import rowley_message
-from badger import garrettbadger
+from badger import garrettbadger, random
 from spencer import my_message
 from yeaman import my_statement
 print('Welcome to our collaborative program')
@@ -7,10 +7,6 @@ print('Welcome to our collaborative program')
 rowley_message()
 
 garrettbadger()
-<<<<<<< HEAD
 my_message()
 my_statement()
-=======
-
-my_message()
->>>>>>> 641e050d978d53fadd8bffc4084cb20c07818e14
+random()
