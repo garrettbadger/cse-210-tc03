8d091a76edaa821de5340dea2737ada131bf23bb
Corey Yeaman <coreyyea@gmail.com>
2021-10-01 11:29:23 -0600

    update

--- welcome/main.py
+++ welcome/main.py
@@ -1,9 +1,11 @@
 from rowley import rowley_message
 from badger import garrettbadger
 from spencer import my_message
+from yeaman import my_statement
 print('Welcome to our collaborative program')
 
 rowley_message()
 
 garrettbadger()
 my_message()
+my_statement()
